af8b6174f1e5a1cc42a8e81e744eac1f4e9a42cf
Francesco Sangalli <francesco.sangalli89@gmail.com>
2022-07-06 17:27:57 +0200

    finish: bonus 1 & 2

---
+++ BONUS/js/main.js
@@ -0,0 +1,26 @@
+
+const containerDom = document.getElementById('container');
+
+for ( let i = 1; i <= 100; i++) {
+    console.log(i);
+
+    const box = document.createElement('div');
+
+    if (i % 5 == 0 && i % 3 == 0) {
+        box.append('fizzbuzz');
+        box.classList = "box fizzBuzz";
+        containerDom.append(box);
+    } else if (i % 3 == 0) {
+        box.append('fizz');
+        box.classList = "box fizz";
+        containerDom.append(box);
+    } else if (i % 5 == 0) {
+        box.append('buzz');
+        box.classList = "box buzz";
+        containerDom.append(box);
+    } else {
+        box.append(i);
+        box.classList = "box";
+        containerDom.append(box);
+    }
+}
