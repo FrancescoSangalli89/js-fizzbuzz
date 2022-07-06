b8ee013f75d3a01eafc7a0f53228b06573ff2676
Francesco Sangalli <francesco.sangalli89@gmail.com>
2022-07-06 16:59:10 +0200

    finish: part1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 
+const containerDom = document.getElementById('container');
+
+for ( let i = 1; i <= 100; i++) {
+    console.log(i);
+
+    if (i % 5 == 0 && i % 3 == 0) {
+        containerDom.innerHTML += `<div class="box">fizzbuzz</div>`;
+    } else if (i % 3 == 0) {
+        containerDom.innerHTML += `<div class="box">fizz</div>`;
+    } else if (i % 5 == 0) {
+        containerDom.innerHTML += `<div class="box">buzz</div>`;
+    } else {
+        containerDom.innerHTML += `<div class="box">${i}</div>`;
+    }
+}
