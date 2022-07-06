6bc3d221ed8f471a5eaf1b87400a31d6320cb7c7
Francesco Sangalli <francesco.sangalli89@gmail.com>
2022-07-06 21:05:59 +0200

    corrected code

--- js/main.js
+++ js/main.js
@@ -5,11 +5,11 @@ for ( let i = 1; i <= 100; i++) {
     console.log(i);
 
     if (i % 5 == 0 && i % 3 == 0) {
-        containerDom.innerHTML += `<div class="box">fizzbuzz</div>`;
+        containerDom.innerHTML += `<div class="box fizzBuzz">fizzbuzz</div>`;
     } else if (i % 3 == 0) {
-        containerDom.innerHTML += `<div class="box">fizz</div>`;
+        containerDom.innerHTML += `<div class="box fizz">fizz</div>`;
     } else if (i % 5 == 0) {
-        containerDom.innerHTML += `<div class="box">buzz</div>`;
+        containerDom.innerHTML += `<div class="box buzz">buzz</div>`;
     } else {
         containerDom.innerHTML += `<div class="box">${i}</div>`;
     }
